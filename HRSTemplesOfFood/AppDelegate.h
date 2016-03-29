6971fafe9e2a30a2eec21f61dfc7a78bf9cbe740
Sebastian Osthoff <sebastian.osthoff@hrs.de>
2016-03-29 10:37:22 +0200

    Initial Commit

---
+++ HRSTemplesOfFood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HRSTemplesOfFood
+//
+//  Created by Sebastian Osthoff on 29/03/16.
+//  Copyright © 2016 HRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HRSTemplesOfFood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HRSTemplesOfFood
+//
+//  Created by Sebastian Osthoff on 29/03/16.
+//  Copyright © 2016 HRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
